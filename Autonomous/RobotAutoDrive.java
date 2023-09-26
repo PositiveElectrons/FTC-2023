cea3002ff769bb687c3a2fbd842f71d1c26ba6c9
PositiveElectrons <146140173+PositiveElectrons@users.noreply.github.com>
2023-09-26 23:30:26 +0000

    Uploaded Autonomous

---
+++ Autonomous/RobotAutoDrive.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.FTCMain.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+@Autonomous(name="Autonomous1")
+
+public class RobotAutoDrive extends LinearOpMode {
+
+    // todo: write your code here
+    
+    @Override
+    public void runOpMode() {
+        
+    }
+}
